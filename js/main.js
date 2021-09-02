f66d86c224eb00b5fe78f3328ea6d995289d44e2
mohon63 <mohonhossen63@gmail.com>
2021-09-02 23:56:45 +0600

    mm

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const searchBook = () => {
         document.getElementById('search-result').textContent = '';
     }
     else {
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
